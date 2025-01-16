716a12fe182d0804928d77fa68a835a24a6e6104
Anton Norgren <vessbon@gmail.com>
2025-01-16 23:33:19 +0100

    Added authentication pages

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, url_for, redirect
 from flask_session import Session
 from flask_socketio import SocketIO
+from functools import wraps
 from redis import Redis
 from dotenv import load_dotenv
 from markupsafe import escape
 
-import os.path
 
-
+# ---> ENVIRONMENT
 load_dotenv()
 
+# ---> APP
 app = Flask(__name__)
+app.secret_key = 'PAFja89wfaIUO'
 
 SESSION_TYPE = 'redis'
 SESSION_REDIS = Redis(host='localhost', port=8000)
@@ -18,19 +20,46 @@ app.config.from_object(__name__)
 Session(app)
 
 
+# ---> MIDDLEWARE
+#  Authentication check
+def require_auth(original_function):
+    @wraps(original_function)
+    def decorated_function(*args, **kwargs):
+        if 'auth' not in session or not session['auth']:
+            return redirect(url_for('login'))
+        return original_function(*args, *kwargs)
+    return decorated_function
+
+# Admin check
+def require_admin(original_function):
+    @wraps(original_function)
+    def decorated_function(*args, **kwargs):
+        if 'admin' not in session or not session['admin']:
+            return redirect(url_for('forbidden'))
+        return original_function(*args, *kwargs)
+    return decorated_function
+
+
+# ---> ROUTES
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/set/')
-def session_set():
-    session['key'] = 'value'
-    return 'ok'
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
-@app.route('/get/')
-def session_get():
-    return session.get('key', 'not set')
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/dashboard')
+@require_auth
+@require_admin
+def dashboard():
+    pass
 
 
+# ---> LOGIC
 if __name__ == '__main__':
     app.run(debug=True)
